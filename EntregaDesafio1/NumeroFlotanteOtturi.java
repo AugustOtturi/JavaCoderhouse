a0ed448b38a07d2b136b699fe75fe1b6bacf3baa
Augusto Otturi <augustotturi.dev@gmail.com>
2023-08-16 21:16:09 -0600

    entrega final 1

---
+++ EntregaDesafio1/NumeroFlotanteOtturi.java
@@ -0,0 +1,17 @@
+package EntregaDesafio1;
+
+//Classe
+public class NumeroFlotanteOtturi {
+    //Se crea el método
+    public static void main(String[] args) {
+        //Entero
+        int numeroUno= 25;
+        //Float
+        float numeroDos = numeroUno;
+
+        //Resultados
+        System.out.println("El numero entero es: " + numeroUno );
+        System.out.println("El numero float es: " + numeroDos );
+
+    }
+}
