20379893f723495ec76631952739c981b87ffa18
Augusto Otturi <117783065+AugustOtturi@users.noreply.github.com>
2023-09-13 20:09:16 -0600

    Add files via upload

--- Entrega2Personas/Test.java
+++ Entrega2Personas/Test.java
@@ -7,9 +7,9 @@ public class Test {
 
     public static void main(String[] args) {
         // Construye los objetos
-        Personas persona1 = new Personas("Augusto", "Otturi");
-        Personas persona2 = new Personas("Ricardo", "Perez");
-        Personas persona3 = new Personas("Juana", "Molina");
+        Personas persona1 = new Personas("Octavio", "Otturi");
+        Personas persona2 = new Personas("Walter", "Perez");
+        Personas persona3 = new Personas("Juana", "Astarte");
         Personas persona4 = new Personas("Ricardo", "Mollo");
         Personas persona5 = new Personas("Pity", "Fernandez");
 
@@ -22,13 +22,34 @@ public class Test {
         listaPersonas.add(persona5);
 
         // Ordena la lista de personas
-
         System.out.println("Inicio del programa");
         System.out.println("--------------------");
-        System.out.println("Personas ordenadas por apellido y nombre:");
+        System.out.println("--------------------");
 
-        for (Personas persona : listaPersonas) {
-            System.out.println(persona.getApellido() + ", " + persona.getNombre());
+        System.out.println("Orden según nombres");
+        listaPersonas.sort((p1, p2) -> p1.getApellido().compareTo(p2.getApellido()));
+        listaPersonas.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+        for (Personas PersonasArray : listaPersonas) {
+            System.out.println(PersonasArray);
+        }
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+
+        System.out.println("Orden según Apellidos");
+        listaPersonas.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+        listaPersonas.sort((p1, p2) -> p1.getApellido().compareTo(p2.getApellido()));
+        for (Personas PersonasArray : listaPersonas) {
+            System.out.println(PersonasArray);
+
+        }
+        System.out.println("--------------------");
+        System.out.println("--------------------");
+
+        System.out.println("Orden inverso según Apellidos");
+        listaPersonas.sort((p1, p2) -> p2.getNombre().compareTo(p1.getNombre()));
+        listaPersonas.sort((p1, p2) -> p2.getApellido().compareTo(p1.getApellido()));
+        for (Personas PersonasArray : listaPersonas) {
+            System.out.println(PersonasArray);
         }
     }
 }
